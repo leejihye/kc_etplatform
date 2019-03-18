b90b4465ef0131342dd293a4726e609f66f05a57
threeon <ibksyong@gmail.com>
2019-03-18 20:14:08 +0900

    회사 20:00

--- etc/db/SQL/CREATE/COMMON_CODE.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `COMMON_CODE` (
-`cmm_cd`	CHAR(6)	NOT NULL COMMENT	'기관분류코드',
-`inst_type_cd`	CHAR(4)	NOT NULL COMMENT	'기관분류코드',
-`inst_type_name`	VARCHAR(40)	NOT NULL COMMENT	'기관분류명명',
-PRIMARY KEY (`cmm_cd`,`inst_type_cd`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='COMMON_CODE';
--- etc/db/SQL/CREATE/DOMAIN_MAST.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `DOMAIN_MAST` (
-`inst_type_cd`	CHAR(4)	NOT NULL COMMENT	'기관분류코드',
-`inst_type_name`	VARCHAR(40)	NOT NULL COMMENT	'기관분류명명',
-`inst_cd`	CHAR(5)	NOT NULL COMMENT	'기관구분코드',
-`inst_name`	VARCHAR(40)	NOT NULL COMMENT	'기관명',
-`domain`	VARCHAR(40)	COMMENT	'도메인명',
-PRIMARY KEY (`inst_type_cd`, `inst_cd`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='DOMAIN_MAST';
